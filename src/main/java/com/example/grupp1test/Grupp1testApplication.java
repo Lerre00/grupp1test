4d35c6792a1bc0c5004c0004548f7fa3d2fbe57c
Leroy Collazo <leroycollazo@hotmail.com>
2023-08-22 13:52:19 +0200

    Initial commit

---
+++ src/main/java/com/example/grupp1test/Grupp1testApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupp1test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp1testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupp1testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grupp1test/Grupp1testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupp1test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupp1testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
