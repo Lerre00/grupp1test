62e93cab73c618bd84f74a0789d770fcb9e1f13e
lcj <lcj@kth.se>
2023-09-04 15:15:27 +0200

    added new way to bruteforce

--- src/main/java/com/example/grupp1test/Grupp1testApplication.java
+++ src/main/java/com/example/grupp1test/Grupp1testApplication.java
@@ -2,6 +2,7 @@ package com.example.grupp1test;
 
 import com.example.grupp1test.Models.Users;
 import com.example.grupp1test.Repositories.UserRepo;
+import com.example.grupp1test.config.SecurityConfig;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,15 +14,15 @@ public class Grupp1testApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Grupp1testApplication.class, args);
-
-
     }
     @Bean
     public CommandLineRunner demo(UserRepo userRepo) {
         return (args) -> {
-            Users user1 = new Users("Kalle", "dawfsfgfvd");
-            Users user2 = new Users("Adam", "fmiesog8314");
-            Users user3 = new Users("Fia", "12345678");
+            SecurityConfig b = new SecurityConfig();
+
+            Users user1 = new Users("Kalle", b.passwordEncoder().encode("123789"));
+            Users user2 = new Users("Adam", b.passwordEncoder().encode("fmiesog8314"));
+            Users user3 = new Users("Fia", b.passwordEncoder().encode("1000"));
 
             userRepo.save(user1);
             userRepo.save(user2);
