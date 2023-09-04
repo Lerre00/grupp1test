1debf147cb7752db3559d327dcc8093a14908c49
lisapel <lisa.peldan@gmail.com>
2023-09-04 10:59:19 +0200

    worthless

--- src/main/java/com/example/grupp1test/Controller/Controller.java
+++ src/main/java/com/example/grupp1test/Controller/Controller.java
@@ -7,7 +7,11 @@ import com.example.grupp1test.SecurityConfig;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 
 @org.springframework.stereotype.Controller
 @RequestMapping
@@ -25,22 +29,22 @@ public class Controller {
     }
 
     @PostMapping("login")
-    public String logIn(Users user, Model model) {
+    public String logIn(Users user, Model model) throws IOException {
         Users dbUser = userRepo.findByName(user.getName());
         SecurityConfig securityConfig = new SecurityConfig();
+        String filePath = "src/main/java/com/example/grupp1test/commonPasswords";
+        Optional<String> foundPassword = Files.lines(Paths.get(filePath))
+                .map(line -> line.split(" ")[0])
+                .filter(password -> dbUser != null && securityConfig.passwordEncoder().matches(password, dbUser.getPassword()))
+                .findFirst();
 
-        int password = 0;
-        while (true) {
-            password = hack(password);
-            System.out.println(password);
-            if (dbUser != null && securityConfig.passwordEncoder().matches(password + "", dbUser.getPassword())) {
-                model.addAttribute("message", "You're logged in!");
-                return "log-in-validator";
-
-            }
+        if (foundPassword.isPresent()) {
+            model.addAttribute("message", "You're logged in!");
+            return "log-in-validator";
+        } else {
+            model.addAttribute("message", "Invalid username or password");
+            return "log-in-validator";
         }
-
-
     }
 
     @RequestMapping("home")
